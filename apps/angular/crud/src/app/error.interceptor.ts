a7d7fb8c75bc6a2ee7d750ce666e35d8573bc743
arqueroretador <roque.macia.rocamora@gmail.com>
2023-11-16 18:40:45 +0100

    feat: added interceptor

---
+++ apps/angular/crud/src/app/error.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, finalize, of } from 'rxjs';
+import { LoaderService } from '../services/loader.service';
+
+@Injectable({ providedIn: 'root' })
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private loaderServ: LoaderService) {}
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      finalize(() => {
+        console.log('Request completed');
+      }),
+      catchError((err) => {
+        console.log('Error caught in interceptor');
+        console.error(err);
+        return of();
+      })
+    );
+  }
+}
