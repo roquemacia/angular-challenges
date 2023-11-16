ef44490c32b5343903ee322e253d3e13f5a648a3
arqueroretador <roque.macia.rocamora@gmail.com>
2023-11-16 20:48:55 +0100

    refactor: interceptor to be a function instead of class

--- apps/angular/crud/src/app/app.config.ts
+++ apps/angular/crud/src/app/app.config.ts
@@ -1,14 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { ErrorInterceptor } from './error.interceptor';
+import { importProvidersFrom } from '@angular/core';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import { basicInterceptor } from './error.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(HttpClientModule),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ErrorInterceptor,
-      multi: true,
-    },
+    provideHttpClient(withInterceptors([basicInterceptor])),
   ],
 };
--- apps/angular/crud/src/app/error.interceptor.ts
+++ apps/angular/crud/src/app/error.interceptor.ts
@@ -1,30 +1,18 @@
-import {
-  HttpEvent,
-  HttpHandler,
-  HttpInterceptor,
-  HttpRequest,
-} from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, catchError, finalize, of } from 'rxjs';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, finalize, of } from 'rxjs';
 import { LoaderService } from '../services/loader.service';
 
-@Injectable({ providedIn: 'root' })
-export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private loaderServ: LoaderService) {}
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    return next.handle(req).pipe(
-      finalize(() => {
-        this.loaderServ.hideLoader();
-        console.log('Request completed');
-      }),
-      catchError((err) => {
-        console.log('Error caught in interceptor');
-        console.error(err);
-        return of();
-      })
-    );
-  }
-}
+export const basicInterceptor: HttpInterceptorFn = function (req, next) {
+  return next(req).pipe(
+    finalize(() => {
+      inject(LoaderService).hideLoader();
+      console.log('Request completed');
+    }),
+    catchError((err) => {
+      console.log('Error caught in interceptor');
+      console.error(err);
+      return of();
+    })
+  );
+};
